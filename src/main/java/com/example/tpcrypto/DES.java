303b54c317df725d97c244cd5a8984ca6c2f4105
Mdurir <dorian.bouteldja@gmail.com>
2023-09-18 11:17:00 +0200

    Implementation Constantes et Attributs

--- src/main/java/com/example/tpcrypto/DES.java
+++ src/main/java/com/example/tpcrypto/DES.java
@@ -6,6 +6,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class DES extends Application {
     @Override
@@ -16,6 +17,20 @@ public class DES extends Application {
         stage.setScene(scene);
         stage.show();
     }
+
+    int taille_bloc = 64;
+    int taille_sous_bloc = 32;
+    int nb_ronde = 1;
+    int tab_decalage = 0;
+    int perme_initiale = 0;
+    ArrayList<Integer> PI = new ArrayList<Integer>();
+    ArrayList<Integer> PC1 = new ArrayList<Integer>();
+    ArrayList<Integer> PC2 = new ArrayList<Integer>();
+    ArrayList<Integer> S = new ArrayList<Integer>();
+    ArrayList<Integer> E = new ArrayList<Integer>();
+
+    int[] masterkey;
+    ArrayList<Integer> tab_cles = new ArrayList<Integer>();
 
     public static void main(String[] args) {
         launch();
