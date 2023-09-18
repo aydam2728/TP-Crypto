fb28b902030439ff1ace19b1c9ba483957786ea8
aydam2728 <lesdmart@gmail.com>
2023-09-18 10:49:20 +0200

    first commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,15 +2,6 @@ module com.example.tpcrypto {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.web;
-
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires net.synedra.validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
-    requires com.almasb.fxgl.all;
-
     opens com.example.tpcrypto to javafx.fxml;
     exports com.example.tpcrypto;
 }
