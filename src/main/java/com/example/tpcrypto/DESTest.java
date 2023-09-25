669c062f3e347579f2e669056c88538b57447f89
aydam2728 <lesdmart@gmail.com>
2023-09-25 11:43:06 +0200

    add jupiter test

---
+++ src/main/java/com/example/tpcrypto/DESTest.java
@@ -0,0 +1,62 @@
+package com.example.tpcrypto;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DESTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void crypte() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void decrypte() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void stringToBits() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void bitsToString() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void decouppage() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void recollage_bloc() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void genereMasterKey() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void permutation() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void invPermutation() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void decalle_gauche() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void xor() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void génèreClé() {
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.example.tpcrypto {
     requires javafx.fxml;
     requires javafx.web;
     requires org.apache.commons.lang3;
+    requires org.junit.jupiter.api;
     opens com.example.tpcrypto to javafx.fxml;
     exports com.example.tpcrypto;
 }
