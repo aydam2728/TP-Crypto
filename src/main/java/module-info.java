e4566549f5a346cd498cbe56d8c2d5cbea2da3e0
Mdurir <dorian.bouteldja@gmail.com>
2023-10-16 12:16:03 +0200

    Faut corrigé Teste :)))

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.tpcrypto {
-    requires org.apache.commons.lang3;
-    requires org.junit.jupiter.api;
-    requires java.desktop;
-    requires java.logging;
-    opens com.example.tpcrypto;
-    exports com.example.tpcrypto;
-}
